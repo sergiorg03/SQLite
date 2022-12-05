02768fbc12ba694eb1ec623056137179dec0fd6a
serrodgen <srodgen0910@g.educaand.es>
2022-12-05 20:53:24 +0100

    Finished all the modificar class methods

--- app/src/main/java/com/example/sqlite/Modificar.java
+++ app/src/main/java/com/example/sqlite/Modificar.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.SQLException;
 
@@ -12,7 +14,11 @@ public class Modificar extends AppCompatActivity {
     MainActivity MA;
     public static DBManager DDBBM;
     private String sql;
-    private int var;
+    private static int numeroEmpleadoModificar;
+
+    TextView numeroEmpleadoModif;
+    TextView apellidoEmpleadoModif;
+    TextView salarioEmpleadoModif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +31,62 @@ public class Modificar extends AppCompatActivity {
             e.printStackTrace();
         }
         sql = "";
+        numeroEmpleadoModif = findViewById(R.id.emp_no_modif);
+        apellidoEmpleadoModif = findViewById(R.id.ModApellido);
+        salarioEmpleadoModif = findViewById(R.id.ModSal);
     }
 
-    private String creacionDeCadenasUpdate(){
-        sql = "UPDATE "+" SET "+" WHERE = "+ this.var +";";
-        return sql;
+
+
+    public void modificarDatos(View view) throws SQLException {
+
+        if (isNumeric(String.valueOf(numeroEmpleadoModif))){
+            if (DDBBM.comprobacionExistencia(numeroEmpleadoModificar)){
+                DDBBM.insert(datoAModif(datoRellenado()));
+            }else{
+                Toast.makeText(this, "El empleado introducido no existe. ", Toast.LENGTH_LONG);
+            }
+        }else{
+            Toast.makeText(this, "Introduzca el numero de empleado correcto. ", Toast.LENGTH_LONG);
+        }
     }
 
-    public void modificarDatos(View view){
-        DDBBM.insert(creacionDeCadenasUpdate());
+    /**
+     *
+     * @param cadena {String} -- Recibe la cadena que se va a comprobar si es un numero
+     * @return {boolean} -- Devuelve true si la cadena es un numero, false si es una cadena.
+     */
+    private static boolean isNumeric(String cadena){
+        try {
+
+            numeroEmpleadoModificar = Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
     }
 
     public void volver (View view){
         onBackPressed();
+    }
+
+    public int datoRellenado(){
+        if (apellidoEmpleadoModif == null){
+            return 1;
+        }else return 2;
+    }
+
+    public String datoAModif(int opcion){
+
+        String cadena = null;
+        switch (opcion) {
+            case 1:
+                cadena = "UPDATE Empleados SET apellido = "+ this.apellidoEmpleadoModif +" WHERE emp_no = "+ this.numeroEmpleadoModificar +";";
+                break;
+            case 2:
+                cadena = "UPDATE Empleados SET salario = "+ this.salarioEmpleadoModif +" WHERE emp_no = "+ this.numeroEmpleadoModificar +";";
+                break;
+        }
+        return cadena;
     }
 }
